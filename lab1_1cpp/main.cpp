98628c39f5176053ccab2bab528f748723a1e0a8
Maksym Galchuk <maksymgalch@gmail.com>
2023-02-21 18:19:45 +0200

    Print coordinates

--- lab1_1cpp/main.cpp
+++ lab1_1cpp/main.cpp
@@ -1,11 +1,14 @@
 ﻿#include "Rectangle.h"
-#include <iostream>
+#include <cstdio>
 
 int main() {
     Rectangle r1(0, 0, 5, 10);
-    std::cout << "Width: " << r1.width() << std::endl;
-    std::cout << "Height: " << r1.height() << std::endl;
-    std::cout << "Area: " << r1.area() << std::endl;
-    std::cout << "Perimeter: " << r1.perimeter() << std::endl;
+    printf("Coordinates: (%.1f; %.1f), (%.1f; %.1f), (%.1f; %.1f), (%.1f; %.1f) \n",
+        r1.getX1(), r1.getY1(), r1.getX1(), r1.getY2(), r1.getX2(), r1.getY2(), r1.getX2(), r1.getY1());
+    printf("Width: %.1f\n", r1.width());
+    printf("Height: %.1f\n", r1.height());
+    printf("Area: %.1f\n", r1.area());
+    printf("Perimeter: %.1f\n", r1.perimeter());
+    
     return 0;
 }
--- lab1_1cs/Program.cs
+++ lab1_1cs/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace lab1_1cs {
     class Program {
         static public void Main(String[] args) {
-  
             Rectangle r1 = new Rectangle(0, 0, 5, 10);
+            Console.WriteLine($"Coordinates: ({r1.X1}; {r1.Y1}), ({r1.X1}; {r1.Y2}), ({r1.X2}; {r1.Y2}), ({r1.X2}; {r1.Y1})");
             Console.WriteLine("Width: " + r1.Width);
             Console.WriteLine("Height: " + r1.Height);
             Console.WriteLine("Area: " + r1.Area());
